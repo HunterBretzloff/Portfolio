c0e276da11d615ea676893b039bb9b4001f92877
HunterBretzloff <cyph3rnetworks@gmail.com>
2026-04-30 23:48:38 -0400

    Add files via upload

---
+++ projects/python_practice/band_name_generator/band_name_generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+
+city_Name = input("What city are you in?: ")
+pet_Name = input("What is your pet's name?: ")
+
+print(f"Your band name could be: {city_Name} {pet_Name}")
